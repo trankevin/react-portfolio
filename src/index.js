99999ec2dcdfbd8e1df3c29633f1000c08bb15f2
Kevin Tran <tran.kevin.j@gmail.com>
2023-01-17 14:54:05 -0500

    Add tag manager

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'G-BVTV109F6J'
+}
+
+if (process.env.NODE_ENV == 'production') {
+  TagManager.initialize(tagManagerArgs)
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
